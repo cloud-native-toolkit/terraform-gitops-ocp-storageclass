0bd42a310b4297c9fc5dfc30b4968b47b1232b99
Thomas Skill <tskill@us.ibm.com>
2021-12-03 15:22:21 -0600

    nmsp add

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource null_resource setup_gitops {
   depends_on = [null_resource.create_yaml]
 
   provisioner "local-exec" {
-    command = "${local.bin_dir}/igc gitops-module '${local.name}' --contentDir '${local.yaml_dir}' --serverName '${var.server_name}' -l '${local.layer}' --debug"
+    command = "${local.bin_dir}/igc gitops-module '${local.name}' -n 'notneeded' --contentDir '${local.yaml_dir}' --serverName '${var.server_name}' -l '${local.layer}' --debug"
 
     environment = {
       GIT_CREDENTIALS = yamlencode(var.git_credentials)

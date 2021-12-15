d168e91821d5739333d3f375ac84ac287db3940f
Tom Skill <tskill@us.ibm.com>
2021-12-15 16:38:33 -0600

    Timeout (#4)
    * rdmeup
    
    * waveadd
    
    * increasetime

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -40,13 +40,13 @@ echo "Printing payload/${LAYER}/namespace/${NAMESPACE}/${COMPONENT_NAME}/sc.yaml
 cat "payload/${LAYER}namespace/${NAMESPACE}/${COMPONENT_NAME}/sc.yaml"
 
 count=0
-until kubectl get StorageClass ${STORNAME} || [[ $count -eq 10 ]]; do
+until kubectl get StorageClass ${STORNAME} || [[ $count -eq 15 ]]; do
   echo "Waiting for StorageClass ${STORNAME} to deploy"
   count=$((count + 1))
   sleep 15
 done
 
-if [[ $count -eq 10 ]]; then
+if [[ $count -eq 15 ]]; then
   echo "Timed out waiting for StorageClass ${STORNAME} to deploy"
   exit 1
 fi
